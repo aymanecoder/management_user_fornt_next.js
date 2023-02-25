5265d44ead3d07fb4e8d144e218f6a6263322ad4
aymanecoder <aymanbiti2@gmail.com>
2023-02-25 23:19:32 +0100

    edit

--- components/User.jsx
+++ components/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function User({user ,deleteUser, updateUser}) {
+export default function User({user ,deleteUser, editUser}) {
   return (
 
                     <tr key={user.id}>
@@ -14,7 +14,7 @@ export default function User({user ,deleteUser, updateUser}) {
                             <div className="text-sm text-gray-500">{user.emailId}</div>
                         </td>
                         <td className="text-left px-6 py-4 whitespace-nowrap">
-                           <a onClick={(e,id) => updateUser(e,user.id)} className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer px-4">Edit</a>
+                           <a onClick={(e, id) => editUser(e, user.id)} className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer px-4">Edit</a>
                            <a onClick={(e,id) => deleteUser(e,user.id)} className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer">Delete</a>
 
                         </td>
--- components/UserList.jsx
+++ components/UserList.jsx
@@ -1,83 +1,92 @@
-import { useEffect, useState } from 'react';
-import User from './User'
+
+import { React, useState, useEffect } from "react";
+import EditUser from "./EditUser";
+import User from "./User";
 
 const UserList = ({ user }) => {
-    const USER_API_BASE_URL = "http://localhost:8082/api/v1/users";
-    const [users,setUsers]=useState(null);
-    const [loading,setLoading] = useState(true);
-    const [userId, setUserId] = useState(null)
-    useEffect(()=>{
-        const fetchData = async()=>{
-            setLoading(true);
-            try {
-                const response = await fetch(USER_API_BASE_URL,{
-                    method: 'GET',
-                    headers: {
-                        "content-type": "application/json",
-                    },
-                });
-                
-                const users =await response.json();
-                console.log(users);
-                setUsers(users);
-            }catch(e){
-                console.log(e);
+  const USER_API_BASE_URL = "http://localhost:8082/api/v1/users";
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userId, setUserId] = useState(null);
+  const [responseUser, setResponseUser] = useState(null);
 
-
-            }
-            setLoading(false);
-        };
-        fetchData();
-    },[user]);
-
-    const deleteUser =(e,id)=>{
-        e.preventDefault();
-        fetch(USER_API_BASE_URL + '/' + id,{
-            method: 'DELETE',
-
-        }).then((res)=>{
-            if(users){
-                setUsers((prevElement)=>{
-                    return prevElement.filter((user)=>user.id !== id);
-
-                });
-
-            }
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(USER_API_BASE_URL, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
-        const updateUser =(e,id)=>{
-            e.preventDefault();
-            setUserId(id);
-            
-        }
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [user, responseUser]);
 
-    }
-    return ( 
-    <div className="container mx-auto my-8">
+  const deleteUser = (e, id) => {
+    e.preventDefault();
+    fetch(USER_API_BASE_URL + "/" + id, {
+      method: "DELETE",
+    }).then((res) => {
+      if (users) {
+        setUsers((prevElement) => {
+          return prevElement.filter((user) => user.id !== id);
+        });
+      }
+    });
+  };
+
+  const editUser = (e, id) => {
+    e.preventDefault();
+    setUserId(id);
+  };
+
+  return (
+    <>
+      <div className="container mx-auto my-8">
         <div className="flex shadow border-b">
-            <table className="min-w-full">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th className="text-left font-medium text-gray-500 upprcase tracking-wide py-3 px-6">First Name</th>
-                        <th className="text-left font-medium text-gray-500 upprcase tracking-wide py-3 px-6">Last Name</th>
-                        <th className="text-left font-medium text-gray-500 upprcase tracking-wide py-3 px-6">EmailId</th>
-                        <th className="text-left font-medium text-gray-500 upprcase tracking-wide py-3 px-6">Action</th>
-                    </tr>
-                </thead>
-               {!loading ?( <tbody className="bg-white">
-                   { users.map((user)=>(
-                        <User user={user} key={user.id}  deleteUser={deleteUser} updateUser={updateUser}/>
-                    )
-                    )}
-                 </tbody>) : null }
-               
-                       
-            </table>
-
-
+          <table className="min-w-full">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="text-left font-medium text-gray-500 uppercase tracking-wide py-3 px-6">
+                  First Name
+                </th>
+                <th className="text-left font-medium text-gray-500 uppercase tracking-wide py-3 px-6">
+                  Last Name
+                </th>
+                <th className="text-left font-medium text-gray-500 uppercase tracking-wide py-3 px-6">
+                  EmailId
+                </th>
+                <th className="text-left font-medium text-gray-500 uppercase tracking-wide py-3 px-6">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            {!loading && (
+              <tbody className="bg-white">
+                {users?.map((user) => (
+                  <User
+                    user={user}
+                    key={user.id}
+                    deleteUser={deleteUser}
+                    editUser={editUser}
+                  />
+                ))}
+              </tbody>
+            )}
+          </table>
         </div>
+      </div>
+      <EditUser userId={userId} setResponseUser={setResponseUser} />
+    </>
+  );
+};
 
-
-    </div> );
-}
- 
 export default UserList;
